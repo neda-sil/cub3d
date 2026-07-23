905bb9e5a2586070f88e88661e33baf6fa646951
malaimo <malaimo@f2r7s20.paris.42.school>
2026-07-23 11:53:42 +0200

    rajouts calculs

--- src/calculs/raycasting.c
+++ src/calculs/raycasting.c
@@ -1,7 +1,13 @@
 #include "../../includes/cub3d.h"
 
+void	init_rays(t_ryct	ray)
+{
+	
+}
+
 void	init_calculs(t_data *data, t_ryct ray)
 {
 	ray.x_camera = (-ray.y_dir) * 0.80;
 	ray.y_camera = (ray.x_dir) * 0.80;
+	init_rays(ray);
 }
