81440f4a08ab1a35876c4bafe2cb745b8615fd5d
malaimo <malaimo@f1r9s20.paris.42.school>
2026-07-30 14:53:24 +0200

    bug resolu

--- src/mlx/movements.c
+++ src/mlx/movements.c
@@ -6,7 +6,7 @@
 /*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 15:07:52 by malaimo           #+#    #+#             */
-/*   Updated: 2026/07/30 13:39:25 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/07/30 14:51:57 by malaimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ void	advance(t_data *data, t_ryct *ray)
 	double	x;
 	double	y;
 
-	x = ray->x_player + ray->x_dir / 50;
-	y = ray->y_player + ray->y_dir / 50;
-	if (data->map[(int)y][(int)x] != '1' && data->map[(int)y][(int)x] != ' ')
+	x = ray->x_player + ray->x_dir / 45;
+	y = ray->y_player + ray->y_dir / 45;
+	if (data->map[(int)y][(int)x] != '1' 
+		&& data->map[(int)y][(int)x] != ' ')
 	{
-		ray->x_player = x;
-		ray->y_player = y;
+		ray->x_player = ray->x_player + ray->x_dir / 50;;
+		ray->y_player = ray->y_player + ray->y_dir / 50;
 		ray->moved = 1;
 		// printf("player is now at [%f][%f]\n", ray->y_player, ray->x_player);
 	}
@@ -35,7 +36,8 @@ void	go_back(t_data *data, t_ryct *ray)
 
 	x = ray->x_player - ray->x_dir / 50;
 	y = ray->y_player - ray->y_dir / 50;
-	if (data->map[(int)y][(int)x] != '1' && data->map[(int)y][(int)x] != ' ')
+	if (data->map[(int)y][(int)x] != '1' 
+		&& data->map[(int)y][(int)x] != ' ')
 	{
 		ray->x_player = x;
 		ray->y_player = y;
