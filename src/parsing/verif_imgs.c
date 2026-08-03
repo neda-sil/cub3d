cc3909730cc2cd1bf22be89d4dee16b611682204
neda-sil <neda-sil@student.42.fr>
2026-08-03 11:26:32 +0200

    verif des images corrige

--- src/parsing/verif_imgs.c
+++ src/parsing/verif_imgs.c
@@ -6,7 +6,7 @@
 /*   By: neda-sil <neda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 11:19:04 by neda-sil          #+#    #+#             */
-/*   Updated: 2026/07/29 12:29:31 by neda-sil         ###   ########.fr       */
+/*   Updated: 2026/08/03 11:25:47 by neda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ void	verif_imgs(t_data *data)
 	int	fd;
 
 	fd = open(data->no, O_RDONLY);
-	if (!fd)
+	if (fd <= 0)
 		handle_exit(data, NO_IMG);
 	close(fd);
 	fd = open(data->so, O_RDONLY);
-	if (!fd)
+	if (fd <= 0)
 		handle_exit(data, NO_IMG);
 	close(fd);
 	fd = open(data->we, O_RDONLY);
-	if (!fd)
+	if (fd <= 0)
 		handle_exit(data, NO_IMG);
 	close(fd);
 	fd = open(data->ea, O_RDONLY);
-	if (!fd)
+	if (fd <= 0)
 		handle_exit(data, NO_IMG);
 	close(fd);
 }
